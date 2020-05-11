5808fd5e522e8cb52032e5ffb6758454f77ed138
William Garner <william.garner7@gmail.com>
2020-05-11 15:19:55 -0700

    better cout

--- mpi.cpp
+++ mpi.cpp
@@ -32,18 +32,18 @@ float PROBABILITY_OF_DEATH; //should be a float between 0 and 100000
 int t; //time variable is global so it can be an extern
 
 int main(int argc, char ** argv) {
-cout << omp_get_max_threads() << endl;
-#pragma omp parallel 
-{
-	cout << omp_get_thread_num() << endl;
-}
+//cout << omp_get_max_threads() << endl;
+//#pragma omp parallel
+//{
+//	cout << omp_get_thread_num() << endl;
+//}
 //    std::ofstream fout("serialData.csv");
 //    if(fout.fail())
 //    {
 //        std::cerr << "Could not open data file! Please close serialData.csv and try again" << std::endl;
 //        return -1;
 //    }
-//    std::cout << "Starting simulation" << std::endl;
+    std::cout << "Starting simulation" << std::endl;
     Timer timer(true);
     srand(time(nullptr));
 
